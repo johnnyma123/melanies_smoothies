d00a6c48edefd9cfdee6b32b3e0f3e53332abe4c
johnnyma123 <41525855+johnnyma123@users.noreply.github.com>
2026-04-02 13:33:10 -0500

    Add pandas import to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import requests  
 from snowflake.snowpark.functions import col
+import pandas as pd
 
 # Write directly to the app.
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
